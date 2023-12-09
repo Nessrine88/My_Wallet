b0b65a52de022c7d9877547fad3d69ab273d84c7
Nessrine Macherki <macherki.nessrine@yahoo.com>
2023-12-09 17:29:16 +0100

    Revert "Authorization and authentification"

--- app/models/entity.rb
+++
@@ -1,7 +0,0 @@
-class Entity < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'author_id'
-    has_many :groups, dependent: :destroy
-    validates :name, :amount, presence: true
-    validates :amount, numericality: { greater_than_or_equal_to: 1 }
-     end
-     
--- app/models/group.rb
+++
@@ -1,8 +0,0 @@
-class Group < ApplicationRecord
-    belongs_to :user
-    has_many :entities
-    belongs_to :users, foreign_key: 'user_id'
-    
-    validates :name, presence: true
-  end
-  
